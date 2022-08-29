d2e9e40fc27fefc2bf8f5bc1733e8f4305895eb8
duong_thuong <duongthuong654@gmail.com>
2022-08-29 19:34:35 +0700

    auth controller

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.scxbxju.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log("connect db successfully!");
   } catch (error) {
