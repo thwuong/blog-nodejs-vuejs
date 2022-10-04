e4dc2433bc5df71f9c604e5467b8074716156e0a
duong_thuong <duongthuong654@gmail.com>
2022-10-04 15:56:53 +0700

    create client

--- routes/commentRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express").Router();
-const CommentController = require("../controllers/CommentController");
-const passport = require("../configs/passport");
-
-Router.post("/:blogId/newcomment", passport, CommentController.createComment);
-Router.delete("/:blogId/delete/:id", passport, CommentController.removeComment);
-
-module.exports = Router;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const authRouter = require("./authRouter");
-const blogRouter = require("./blogRouter");
-const commentRouter = require("./commentRouter");
-module.exports = (app) => {
-  app.use("/api/auth", authRouter);
-  app.use("/api/blog", blogRouter);
-  app.use("/api/comment", commentRouter);
-};
